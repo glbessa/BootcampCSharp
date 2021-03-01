22f5426b08f9ca1239ba07f0a47f2d05fd428c89
glbessa <gabrielleitebessa@gmail.com>
2021-03-01 16:38:24 -0300

    Iniando a partir daqui

---
+++ EstruturaDePrograma/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using EstruturaDePrograma.Utils;
+
+namespace EstruturaDePrograma
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = new Pilha();
+            s.Empilha(1);
+            s.Empilha(2);
+            s.Empilha(3);
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+            Console.WriteLine(s.Desempilha());
+        }
+    }
+}
